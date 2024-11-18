89c204550ac7ade6777a80dbb19c90b590d9357c
alexpavlyuk707 <alex.pavlyuk90@icloud.com>
2024-11-17 22:07:48 -0700

    adding new text content to the file Person

--- mySwifyProject/ViewController.swift
+++ mySwifyProject/ViewController.swift
@@ -12,16 +12,28 @@ class ViewController: UIViewController {
     let name = "Aleksey"
     let lastName = "Pavlyuk"
     
+    let helper = Helper()
+    
     var fullName: String {
         "\(name) \(lastName)"
     }
     
     let person = Person(name: "Aleksey", lastName: "Pavlyuk")
     
+    let personOne = Person(name: "John", lastName: "Smith")
+    let personTwo = Person(name: "Jane", lastName: "Doe")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        helper.addPerson(personOne)
+        helper.addPerson(personTwo)
         
+        let names = helper.getPeople()
+        
+        for person in names {
+          print("\(person.name) \(person.lastName)")
+        }
         
         print(person.fullName)
         
